d684fcbfb47423a9f44c53ed87f30d52b13b3e9e
agiratech-arokiavimalraj <154521894+agiratech-arokiavimalraj@users.noreply.github.com>
2024-02-21 15:26:56 +0530

    done

---
+++ TeamShuffler.java
@@ -0,0 +1,17 @@
+
+public class TeamShuffler {
+    boolean finder;
+    String[] playerName;
+    int playerCount;
+
+    public TeamShuffler() {
+    }
+
+    public TeamShuffler(boolean finder, String[] playerName, int playerCount) {
+        this.finder = finder;
+        this.playerName = playerName;
+        this.playerCount = playerCount;
+    }
+}
+
+
